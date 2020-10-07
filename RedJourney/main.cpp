746b092d5247c11148863a59a4e5a3de3d6eb560
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-07 16:35:28 +0700

    add resize report

--- RedJourney/main.cpp
+++ RedJourney/main.cpp
@@ -1,24 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1080, 720), "Red Journey");
-	sf::CircleShape shape(540.f);
-	shape.setFillColor(sf::Color::Magenta);
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+	sf::RenderWindow window(sf::VideoMode(1080, 720),"Red Adventure",sf::Style::Close|sf::Style::Resize);
+	while (window.isOpen()) {
+
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+			switch (evnt.type) {
+			case sf::Event::Closed:
 				window.close();
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-			{
-				window.close();
+					break;
+			case sf::Event::Resized:
+					std::cout << "New window width : " << evnt.size.width << " New window height : " << evnt.size.height << std::endl;
+					break;
 			}
+			/*if (evnt.type == evnt.Closed) {
+				window.close();
+			}*/
 		}
-		window.clear();
-		window.draw(shape);
-		window.display();
+
 	}
 	return 0;
+
 }
