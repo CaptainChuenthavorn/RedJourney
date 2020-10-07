51e3675a4dbc2a58f6343dc67a6f424e8402c971
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-07 21:37:06 +0700

    will add face right

--- RedJourney/animation.h
+++ RedJourney/animation.h
@@ -6,7 +6,7 @@ public:
 	animation(sf::Texture* texture,sf::Vector2u imageCount,float switchTime);
 	~animation();
 
-	void Update(int row, float deltaTime);
+	void Update(int row, float deltaTime,bool faceRight);
 
 public:
 	sf::IntRect uvRect;
